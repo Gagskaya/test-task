66558f04cdacbc812942dbc541bbec692b033a9a
Gagskaya <alisxan006@mail.ru>
2020-05-12 22:30:01 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react';
+import axios from 'axios';
+import { connect } from 'react-redux';
+import Container from '@material-ui/core/Container';
+import Paper from '@material-ui/core/Paper';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import { setData } from './actions/setData';
+import './App.scss';
+import Cards from './components/Cards';
 
-function App() {
+
+const App = (props) => {
+  const { items, setData } = props;
+  useEffect(() => {
+    axios.get('/data.json').then(({ data }) => {
+      setData(data);
+    })
+  }, [setData]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="md">
+      <div className="app">
+        <div className="app__sort">
+          <h4 className="app__sort-title">
+            Сортировка
+          </h4>
+          <div className="app__sort-tabs">
+            <span>ID</span>
+            <span>Имя</span>
+            <span>Возраст</span>
+          </div>
+          <div className="app__sort-tabs">
+            <span>По возрастанию</span>
+            <span>По убыванию</span>
+          </div>
+        </div>
+        <div className="app__view">
+          <h4><Cards /></h4>
+
+        </div>
+      </div>
+
+    </Container>
   );
 }
 
-export default App;
+const mapStateToProps = ({ data }) => ({
+  items: data.items
+});
+export default connect(mapStateToProps, { setData })(App);
